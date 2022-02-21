bf98f760659b1f86fd5293f423e4b2dee3fd8c0c
Mirko-r <04mirko@libero.it>
2022-02-21 11:37:18 +0100

    Add arrays

---
+++ arrays.rs
@@ -0,0 +1,9 @@
+use std::mem;
+
+fn main(){
+    let xs: [i32; 5] = [1, 2, 3, 4, 5]; //fixed-size array
+                                        
+
+    //All elements ca be initialized at the same value
+    let ys: [i32; 500] = [0; 500];
+}
